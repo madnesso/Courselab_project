8a9dfbb456e437b475e20368a725ecbc9c51ac3d
Abdullah Mohamed Saad <deadsasuke@gmail.com>
2021-06-14 15:37:54 +0200

    Add files via upload
    adding immune system

---
+++ Respiratory system/2/module.js
@@ -0,0 +1,14 @@
+// File	name: Module.js
+// Created: 01.12.2005
+// Copyright (c) Websoft, 2006.	All rights reserved.
+
+
+// InitModule
+function InitModule()
+{
+}
+
+// ShutdownModule
+function ShutdownModule()
+{
+}
